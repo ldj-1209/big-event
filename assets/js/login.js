64e386da421d56b150b943824cf2fcb6b54e2b2a
ldj <540130049@qq.com>
2020-06-17 11:14:25 +0800

    实现注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -18,37 +18,37 @@ $(function() {
     })
 
     //  控制登录表单
-    $('.layui-form').submit(function(e) {
+    $('#loginForm').submit(function(e) {
 
-        e.preventDefault();
+            e.preventDefault();
 
-        var formData = $(this).serialize();
-
-
+            var formData = $(this).serialize();
 
 
-        $.ajax({
-            type: 'post',
-            url: 'http://ajax.frontend.itheima.net/api/login',
-            data: formData,
-            success: function(res) {
-                console.log(res);
-
-                if (res.status === 0) {
-
-                    console.log(res.token);
-
-                    localStorage.setItem('mytoken', res.token);
 
 
-                    location.href = './index.html'
+            $.ajax({
+                type: 'post',
+                url: 'http://ajax.frontend.itheima.net/api/login',
+                data: formData,
+                success: function(res) {
+                    console.log(res);
+
+                    if (res.status === 0) {
+
+                        console.log(res.token);
+
+                        localStorage.setItem('mytoken', res.token);
+
+
+                        location.href = './index.html'
+                    }
                 }
-            }
+            })
         })
-    })
+        //  控制登录表单 end
 
-    //  控制表单切换
-
+    //  控制表单切换 star
     $('#registered .links').click(function() {
 
         $('#registered').hide()
@@ -56,11 +56,27 @@ $(function() {
     });
     $('#loginForm .links').click(function() {
 
-        $('#loginForm').hide()
-        $('#registered').show()
+            $('#loginForm').hide()
+            $('#registered').show()
+        })
+        //  控制表单切换 end
+
+
+    //  控制注册表单
+    $('#registered').submit(function(e) {
+        e.preventDefault()
+        var formData = $(this).serialize()
+
+        $.ajax({
+            type: 'post',
+            url: 'http://ajax.frontend.itheima.net/api/reguser',
+            data: formData,
+            success: function(res) {
+                if (res.status === 0) {
+                    alert('注册成功')
+                    $('#registered .links').click()
+                }
+            }
+        })
     })
-
-
-
-
 })
