17f9084eee170d523eeaab2b60fac9e2157c436d
ldj <540130049@qq.com>
2020-06-17 19:54:04 +0800

    index退出功能完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,45 @@
 $(function() {
 
+    $.ajax({
+        type: 'get',
+        url: 'http://ajax.frontend.itheima.net/my/userinfo',
+        headers: {
+            Authorization: mytoken
+        },
+        success: function(res) {
+            if (res.status === 0) {
+                console.log(res);
+
+                var username = res.data.username
+                $('.username').html(username)
+
+                var imgurl = res.data.user_pic
+                if (imgurl) {
+                    $('.username').siblings('img').attr('src', imgurl)
+                } else {
+                    $('.username').siblings('img').attr('src', './assets/images/avatar.jpg')
+                }
+            }
+
+        }
+    })
 
 
+    // 控制退出事件
+    $('#close-btn').click(function() {
 
 
+        layer.confirm('真的要退出吗?', {
+            icon: 3,
+            title: '提示'
+        }, function(index) {
+
+            localStorage.removeItem('mytoken')
+
+            location.href = ('./login.html')
+
+
+            layer.close(index);
+        })
+    })
 })
